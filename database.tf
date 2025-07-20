49b411b89af211de7a1e79174bc3081cf03e6906
Timilehin Abodunrin <ezekielabodunrin@gmail.com>
2025-07-20 11:53:10 +0100

    Fix: Declare VPC and update references; fix RDS parameter group and route table

--- database.tf
+++ database.tf
@@ -1,15 +1,26 @@
 # Create database
+resource "aws_db_parameter_group" "custom_mysql" {
+  name        = "custom-mysql8"
+  family      = "mysql8.0"
+  description = "Custom parameter group for MySQL 8.0"
+
+  tags = {
+    Name = "custom-mysql8"
+  }
+}
+
 resource "aws_db_instance" "default" {
-  count             = 2
-  allocated_storage = 20
-  storage_type      = "gp2"
-  engine            = "mysql"
-  engine_version    = "8.0.36"
-  instance_class    = "db.t3.micro"
-  /* name                 = var.db_name  # The name of the database to create when creating the RDS instance */
-  username             = var.db_username
-  password             = var.db_password
-  parameter_group_name = "default.mysql8.0.36"
-  db_subnet_group_name = aws_db_subnet_group.mydb_subnet_group.name
-  skip_final_snapshot  = true
+  count                  = 2
+  identifier             = "terraform-${count.index}"
+  engine                 = "mysql"
+  engine_version         = "8.0.36"
+  instance_class         = "db.t3.micro"
+  allocated_storage      = 20
+  username               = "admin"
+  password               = "admin1234"
+  db_name                = "mydb"
+  db_subnet_group_name   = aws_db_subnet_group.mydb_subnet_group.name
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  skip_final_snapshot    = true
+  parameter_group_name = aws_db_parameter_group.custom_mysql.name
 }
